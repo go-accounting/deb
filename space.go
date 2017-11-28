1f2f9ed75a923c51d6b4cde7858375eb6edfd08c
Marcos César de Oliveira <mcesarhm@gmail.com>
2017-11-28 15:54:35 -0200

    Add conversion functions

--- space.go
+++ space.go
@@ -1,5 +1,7 @@
 package deb
 
+import "time"
+
 type Account uint32
 type Date uint32
 type Moment uint64
@@ -25,4 +27,20 @@ var logger func(string)
 
 func RegisterLogger(l func(string)) {
 	logger = l
+}
+
+func (d Date) ToTime() time.Time {
+	return time.Date(int(d%100000000/10000), time.Month(d%10000/100), int(d%100), 0, 0, 0, 0, time.UTC)
+}
+
+func DateFromTime(t time.Time) Date {
+	return Date(t.Year()*10000 + int(t.Month())*100 + t.Day())
+}
+
+func (m Moment) ToTime() time.Time {
+	return time.Unix(0, int64(m))
+}
+
+func MomentFromTime(t time.Time) Moment {
+	return Moment(t.UnixNano())
 }
